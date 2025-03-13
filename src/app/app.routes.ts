acdce9f72db804d67da152d6b07ab0ac7a22b1d6
KevindeJesusUAA <garcesdiazkevin@gmail.com>
2025-03-13 01:07:33 -0600

    Se agregó el título de la página, un carousel de imagenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { UbicacionComponent } from './ubicacion/ubicacion.component';
+import { PreguntasfrecuentesComponent } from './preguntasfrecuentes/preguntasfrecuentes.component';
 
 
 export const routes: Routes = [
@@ -8,5 +9,6 @@ export const routes: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },  // Ruta predeterminada
     { path: 'inicio', component: InicioComponent },         // Ruta del componente de inicio
     { path: 'ubicacion', component: UbicacionComponent },
+    { path: 'preguntasfrecuentes', component: PreguntasfrecuentesComponent}, 
     { path: '**', redirectTo: 'inicio' }                    // Ruta para páginas no encontradas
 ];
---
+++ src/app/preguntasfrecuentes/preguntasfrecuentes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntasfrecuentes',
+  standalone: true,
+  imports: [],
+  templateUrl: './preguntasfrecuentes.component.html',
+  styleUrl: './preguntasfrecuentes.component.css'
+})
+export class PreguntasfrecuentesComponent {
+
+}
