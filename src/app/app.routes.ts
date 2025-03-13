5d3460920b3911467b48f8e689ec94399f5c9309
KevindeJesusUAA <garcesdiazkevin@gmail.com>
2025-03-13 16:53:43 -0600

    Publicación Github rutas header con modo hash0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
-import { Routes } from '@angular/router';
+/*import { Routes } from '@angular/router';*/
 import { InicioComponent } from './inicio/inicio.component';
 import { UbicacionComponent } from './ubicacion/ubicacion.component';
 import { PreguntasfrecuentesComponent } from './preguntasfrecuentes/preguntasfrecuentes.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 export const routes: Routes = [
@@ -12,3 +14,10 @@ export const routes: Routes = [
     { path: 'preguntasfrecuentes', component: PreguntasfrecuentesComponent}, 
     { path: '**', redirectTo: 'inicio' }                    // Ruta para páginas no encontradas
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],  // Habilita el modo Hash
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
